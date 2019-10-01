476a7ba165fd88a6f02ba25f2a326cc4d4588323
Leonardo Hirooka <leohirooka2012@hotmail.com>
2019-10-01 01:29:56 -0300

    First Commit

---
+++ index.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset='utf-8' />
+    <link href='css/core/main.min.css' rel='stylesheet' />
+    <link href='css/daygrid/main.min.css' rel='stylesheet' />
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+
+    <script src='js/core/main.min.js'></script>
+    <script src='js/interaction/main.min.js'></script>
+    <script src='js/daygrid/main.min.js'></script>
+    <script src='js/core/locales/pt-br.js'></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+    <script>
+        document.addEventListener('DOMContentLoaded', function() {
+            var calendarEl = document.getElementById('calendar');
+
+            var calendar = new FullCalendar.Calendar(calendarEl, {
+                locale: 'pt-br',
+                plugins: ['interaction', 'dayGrid'],
+                //defaultDate: '2019-04-12',
+                editable: true,
+                eventLimit: true,
+                events: 'list_eventos.php',
+                extraParams: function() {
+                    return {
+                        cachebuster: new Date().valueOf()
+                    };
+                },
+                eventClick: function(info) {
+
+                }
+            });
+
+            calendar.render();
+        });
+    </script>
+    <style>
+        body {
+            margin: 40px 10px;
+            padding: 0;
+            font-family: Arial, Helvetica Neue, Helvetica, sans-serif;
+            font-size: 14px;
+        }
+
+        #calendar {
+            max-width: 900px;
+            margin: 0 auto;
+        }
+    </style>
+</head>
+
+<body>
+
+    <div id='calendar'></div>
+
+</body>
+
+</html>
